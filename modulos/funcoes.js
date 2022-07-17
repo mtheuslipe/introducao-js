4b747cced154712a5659ecdcd7f7ca841918e57d
mtheuslipe <matheusfelipe.mf454@gmail.com>
2022-07-17 19:49:21 -0300

    prática com módulos

---
+++ modulos/funcoes.js
@@ -0,0 +1,14 @@
+function idade(string, num){
+    console.log(`a idade de ${string} é ${num}.`);
+}
+function cidade(nome, cidade){
+    console.log(`a cidade de ${nome} é ${cidade}`);
+}
+function hobby(name, hobby){
+    console.log(`o hobby de ${name} é ${hobby}`);
+}
+export{
+    idade,
+    cidade,
+    hobby,
+};
---
+++ modulos/main.js
@@ -0,0 +1,5 @@
+import * as funcoes from './funcoes.js';
+
+funcoes.idade('Matheus', 18);
+funcoes.hobby('Luiza', 'escrever');
+funcoes.cidade('Thalyta', 'Muriaé-MG')
